18a67c077b33f607b267aae0da51d0df431a6ff3
Raul Gouvea <raul.gouvea@grupocarvalhogestao.com.br>
2025-08-28 16:55:24 -0300

    implementando rotas para listar, atualizar e deletar pátios

--- src/controllers/PatioController.ts
+++ src/controllers/PatioController.ts
@@ -32,4 +32,48 @@ export class PatioController {
       return res.status(400).json({ error: 'Não foi possível criar o pátio.' });
     }
   }
+
+  async list(req: Request, res: Response) {
+    try {
+      const patios = await prismaClient.patio.findMany();
+      return res.status(200).json(patios);
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ error: 'Não foi possível listar os pátios.' });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, address, cep, managerName } = req.body;
+
+    try {
+      const patio = await prismaClient.patio.update({
+        where: { id },
+        data: { name, address, cep, managerName },
+      });
+      return res.status(200).json(patio);
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ error: 'Não foi possível atualizar os dados do pátio.' });
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      await prismaClient.patio.delete({
+        where: { id },
+      });
+
+      return res.status(204).send();
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ error: 'Não foi possível deletar o pátio.' });
+    }
+  }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,13 +9,20 @@ const userController = new UserController();
 const patioController = new PatioController();
 const driverController = new DriverController();
 
+
 // Rotas de User
 routes.post('/users', userController.create);
 
 // Rotas de Patio
 routes.post('/patios', patioController.create);
+routes.get('/patios', patioController.list);
+routes.put('/patios/:id', patioController.update);
+routes.delete('/patios/:id', patioController.delete);
+
 
 // Rotas de Driver
 routes.post('/drivers', driverController.create);
+
+
 
 export default routes;
