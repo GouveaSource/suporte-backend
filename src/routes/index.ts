fa76c67e82089ffe6772f8cef04f46d3794dee2f
Raul Gouvea <raulgouvea@gmail.com>
2025-09-12 17:26:13 -0300

    adicionando rotas de setores

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import { UserController } from '../controllers/UserController';
 import { PatioController } from '../controllers/PatioController';
 import { DriverController } from '../controllers/DriverController';
+import { SetorController } from '../controllers/SetorController';
 
 const routes = Router();
 
 const userController = new UserController();
 const patioController = new PatioController();
 const driverController = new DriverController();
+const setorController = new SetorController();
 
 // Rotas de User
 routes.post('/users', userController.create);
@@ -20,5 +22,11 @@ routes.delete('/patios/:id', patioController.delete);
 
 // Rotas de Driver
 routes.post('/drivers', driverController.create);
+
+// Rotas de Setores
+routes.post('/setores', setorController.create);
+routes.get('/setores', setorController.list);
+routes.put('/setores/:id', setorController.update);
+routes.delete('/setores/:id', setorController.delete);
 
 export default routes;
